6d2c4030ae377e01ff6151a8bd183f284bba314a
emmap88 <emmap8@hotmail.com>
2023-08-15 22:46:59 -0400

    Create timer1.js

---
+++ timer1.js
@@ -0,0 +1,36 @@
+//const prompt = require('prompt-sync')({ sigint: true });
+//process.stdout.write('\x07);
+
+const timer = () => {
+  const args = ["1", "2", "3"];
+  // const args = process.argv.slice(2);
+  let max = maxTime(args);
+  process.stdout.write(max.toString());
+
+  let interval = setInterval(() => {
+    max--;
+    process.stdout.write(`\r${max}`);
+  }, 1000);
+
+  setTimeout(() => {
+    process.stdout.write('\x07');
+    clearInterval(interval);
+  }, max * 1000);
+
+  args.forEach(element => {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, element);
+  });
+};
+
+const maxTime = (args) => {
+  let max = 0;
+  args.forEach(element => {
+    if (parseInt(element) > max) {
+      max = parseInt(element);
+    }
+  });
+  return max;
+};
+timer();
